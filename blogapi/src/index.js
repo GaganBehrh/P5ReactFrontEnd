cf2f90458834c2d122201f1a2574214ddea80479
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-03-13 05:03:10 +0000

    Header and Footer component added and functional

--- blogapi/src/index.js
+++ blogapi/src/index.js
@@ -1,1 +1,25 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const routing = (
+  <Router>
+    <React.StrictMode>
+      <Header />
+      <Route>
+        <Route exact path="/" component={App} />
+      </Route>
+      <Footer />
+    </React.StrictMode>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
+
+serviceWorker.unregister();
